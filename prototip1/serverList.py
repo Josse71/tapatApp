131975ebf5101fd6fa69372f5ff08907d3193007
Josse <jose_99_11@hotmail.com>
2025-02-25 14:35:22 +0100

    corregido

--- prototip1/serverList.py
+++ prototip1/serverList.py
@@ -1,57 +1,63 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 
+app = Flask(__name__)
+
+# Clase User
 class User:
     def __init__(self, id, username, password, email):
         self.id = id
         self.username = username
         self.password = password
         self.email = email
+    
+    def __str__(self):
+        print(self.username+":"+self.password+":"+self.email)
 
-def __str__(self):
-    return "Usuario:" + self.username + " contra:" + self.password + " correo:" + self.email
-
-listUsers= [
-    User(id=1, username="usuari1", password="12345", email="usuari1@gmail.com"),
-    User(id=2, username="usuari2", password="123", email="usuari2@gmail.com"),
-    User(3,"usuari3","00","Miscos@gmail.com")
+# Dades d'exemple
+users = [
+    User(id=1, username="mare", password="mare", email="prova@gmail.com"),
+    User(id=2, username="pare", password="pare", email="prova2@gmail.com")
 ]
 
-for u in listUsers:
-    print(u)
-
-
-class DAOUsers:
+# Classes DAO
+class UserDAO:
     def __init__(self):
-        self.users=listUsers
+        self.users = users
 
-def getUserByUsername(self,username):
+    def get_all_users(self):
+        result = []
+        for user in self.users:
+            result.append(user.__dict__)
+        return result
 
-#encontrar usuario
-    for u in self.users:
-        if u.username == username:
-            return u
+    def get_user_by_username(self, username):
+        for user in self.users:
+            if user.username == username:
+                return user.__dict__
         return None
 
-daousers = DAOUsers()
+# Inicialitzar DAOs
+user_dao = UserDAO()
 
-'''print(daousers.getUserbyUsername("usuario1"))
-u=daousers.getUserByUsername("noencontrao")
-if (u):
-print(u)
-else:
-print("No encontrao")"'''
+# Rutes del Servei Web
+# --------------------
+# Mètodes GET
+# /users: retorna tots els usuaris.
+# /users/username/<string:username>: retorna la informació d'un usuari específic pel seu username.
 
-app = Flask(__name__)
+@app.route('/prototip1/users', methods=['GET'])
+def get_users():
+    return jsonify(user_dao.get_all_users())
 
-@app.route('/tapatapp/getUser',methods=['GET'])
-def getUser():
-    n = str(request.args.get('name'))
-    email = str(request.args.get('email'))
-    return "Hello World Nombre: " + n + " Email: " + email
-
-@app.route('/prototip/getuser/<string:username>',methods=['GET'])
-def prototipGetUser(username):
-    return "Prototip 1, user: " + username
+@app.route('/prototip1/getuser', methods=['GET'])
+def get_user_by_username():
+    username=request.args.get('username', default="", type=str)
+    print("+"+username+"+")
+    user = user_dao.get_user_by_username(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": f"User with username {username} not found"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port="10050")
+    app.run(host='0.0.0.0', port=10050, debug=True)

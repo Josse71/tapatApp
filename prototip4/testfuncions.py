00bdd82fd8ad73ebd8e7f5af815c26a4ed4034bf
Josse <jose_99_11@hotmail.com>
2025-04-02 12:36:07 +0200

    testFuncions

---
+++ prototip4/testfuncions.py
@@ -0,0 +1,12 @@
+def resta(a, b):
+    """Retorna la resta de dos nombres."""
+    return a - b
+
+def divideix(a, b):
+    """Retorna la divisió de dos nombres. Retorna 'Error' si b és 0."""
+    if b == 0:
+        return "Error: divisió per zero"
+    return a / b
+
+print("La resta és: ", resta(2, 3))
+print("La divisió és: ", divideix(2, 0))
